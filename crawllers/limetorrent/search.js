510bd5d5d3b7c8d5e2ca4bcbdeb6f033ce3b83b0
Hirusha-H <77770753+Hirusha-H@users.noreply.github.com>
2021-03-14 16:30:56 +0530

    Update search.js

--- crawllers/limetorrent/search.js
+++ crawllers/limetorrent/search.js
@@ -11,7 +11,7 @@ async function search(search, site = LIMETORRENT_SITE) {
       "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36"
     );
     var page = await browser.newPage();
-    await page.goto(site.replace( var str = "{term}" var res = str.toLowerCase() , search));
+    await page.goto(site.replace( var searchitem = "{term}" var res = searchitem.toLowerCase() , search));
 
     var searchResults = await page.evaluate(async () => {
       var searchResults = document.querySelector("table.table2 > tbody");
