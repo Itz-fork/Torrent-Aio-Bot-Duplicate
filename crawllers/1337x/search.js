eb6af5d7067f00ac91fcb8e1b876ec1e0d466f50
Im-Nexa <87762949+Nexa-Bruh@users.noreply.github.com>
2021-07-26 08:11:47 +0530

    changed 1337x site. hope this works

--- crawllers/1337x/search.js
+++ crawllers/1337x/search.js
@@ -1,5 +1,5 @@
 const puppeteer = require("puppeteer");
-const O337X_SITE = process.env.O337X_SITE || "https://1337x.to/search/{term}/1/";
+const O337X_SITE = process.env.O337X_SITE || "https://www.1337xx.to/search/{term}/1/";
 
 async function search(search, site = O337X_SITE) {
   try {
